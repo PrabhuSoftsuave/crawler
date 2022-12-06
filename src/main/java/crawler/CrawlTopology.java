04238a7f720e604c1260c543e198d50781447e82
prabhunathan-softsuave <prabhunathan.baskar@softsuave.com>
2022-12-06 17:19:46 +0530

    init push

--- src/main/java/crawler/CrawlTopology.java
+++ src/main/java/crawler/CrawlTopology.java
@@ -1,77 +1,123 @@
+//package crawler;
+//
+///**
+// * Licensed to DigitalPebble Ltd under one or more
+// * contributor license agreements.  See the NOTICE file distributed with
+// * this work for additional information regarding copyright ownership.
+// * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+// * (the "License"); you may not use this file except in compliance with
+// * the License.  You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//import com.digitalpebble.stormcrawler.*;
+//import com.digitalpebble.stormcrawler.bolt.FetcherBolt;
+//import com.digitalpebble.stormcrawler.bolt.JSoupParserBolt;
+//import com.digitalpebble.stormcrawler.bolt.SiteMapParserBolt;
+//import com.digitalpebble.stormcrawler.bolt.URLPartitionerBolt;
+//import com.digitalpebble.stormcrawler.indexing.StdOutIndexer;
+//import com.digitalpebble.stormcrawler.persistence.StdOutStatusUpdater;
+//import com.digitalpebble.stormcrawler.spout.MemorySpout;
+//
+//import org.apache.storm.topology.TopologyBuilder;
+//import org.apache.storm.tuple.Fields;
+//
+///**
+// * Dummy topology to play with the spouts and bolts
+// */
+//public class CrawlTopology extends ConfigurableTopology {
+//
+//    public static void main(String[] args) throws Exception {
+//        ConfigurableTopology.start(new CrawlTopology(), args);
+//    }
+//
+//    @Override
+//    protected int run(String[] args) {
+//        TopologyBuilder builder = new TopologyBuilder();
+//
+//        String[] testURLs = new String[] { "http://www.lequipe.fr/",
+//                "http://www.lemonde.fr/", "http://www.bbc.co.uk/",
+//                "http://storm.apache.org/", "http://digitalpebble.com/" };
+//
+//        builder.setSpout("spout", new MemorySpout(testURLs));
+//
+//        builder.setBolt("partitioner", new URLPartitionerBolt())
+//                .shuffleGrouping("spout");
+//
+//        builder.setBolt("fetch", new FetcherBolt()).fieldsGrouping(
+//                "partitioner", new Fields("key"));
+//
+//        builder.setBolt("sitemap", new SiteMapParserBolt())
+//                .localOrShuffleGrouping("fetch");
+//
+//        builder.setBolt("parse", new JSoupParserBolt()).localOrShuffleGrouping(
+//                "sitemap");
+//
+//        builder.setBolt("index", new StdOutIndexer()).localOrShuffleGrouping(
+//                "parse");
+//
+//        Fields furl = new Fields("url");
+//
+//        // can also use MemoryStatusUpdater for simple recursive crawls
+//        builder.setBolt("status", new StdOutStatusUpdater())
+//                .fieldsGrouping("fetch", Constants.StatusStreamName, furl)
+//                .fieldsGrouping("sitemap", Constants.StatusStreamName, furl)
+//                .fieldsGrouping("parse", Constants.StatusStreamName, furl)
+//                .fieldsGrouping("index", Constants.StatusStreamName, furl);
+//
+//        return submit("crawl", conf, builder);
+//    }
+//}
 package crawler;
 
-/**
- * Licensed to DigitalPebble Ltd under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * DigitalPebble licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+// import exception and collection classes
+import java.io.IOException;
+import java.util.HashSet;
+public class CrawlTopology  {
 
-import com.digitalpebble.stormcrawler.*;
-import com.digitalpebble.stormcrawler.bolt.FetcherBolt;
-import com.digitalpebble.stormcrawler.bolt.JSoupParserBolt;
-import com.digitalpebble.stormcrawler.bolt.SiteMapParserBolt;
-import com.digitalpebble.stormcrawler.bolt.URLPartitionerBolt;
-import com.digitalpebble.stormcrawler.indexing.StdOutIndexer;
-import com.digitalpebble.stormcrawler.persistence.StdOutStatusUpdater;
-import com.digitalpebble.stormcrawler.spout.MemorySpout;
+    private HashSet<String> urlLink = new HashSet<>();
 
-import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.tuple.Fields;
+    public static void main(String[] args)  {
+        CrawlTopology obj = new CrawlTopology();
 
-/**
- * Dummy topology to play with the spouts and bolts
- */
-public class CrawlTopology extends ConfigurableTopology {
+        obj.getPageLinks("https://www.javatpoint.com/digital-electronics");
 
-    public static void main(String[] args) throws Exception {
-        ConfigurableTopology.start(new CrawlTopology(), args);
     }
 
-    @Override
-    protected int run(String[] args) {
-        TopologyBuilder builder = new TopologyBuilder();
+    public void getPageLinks(String URL) {
 
-        String[] testURLs = new String[] { "http://www.lequipe.fr/",
-                "http://www.lemonde.fr/", "http://www.bbc.co.uk/",
-                "http://storm.apache.org/", "http://digitalpebble.com/" };
+        if (!urlLink.contains(URL)) {
+            try {
 
-        builder.setSpout("spout", new MemorySpout(testURLs));
+                if (urlLink.add(URL)) {
+                    System.out.println(URL);
+                }
 
-        builder.setBolt("partitioner", new URLPartitionerBolt())
-                .shuffleGrouping("spout");
+                Document doc = Jsoup.connect(URL).get();
 
-        builder.setBolt("fetch", new FetcherBolt()).fieldsGrouping(
-                "partitioner", new Fields("key"));
+                Elements availableLinksOnPage = doc.select("a[href]");
 
-        builder.setBolt("sitemap", new SiteMapParserBolt())
-                .localOrShuffleGrouping("fetch");
+                for (Element ele : availableLinksOnPage) {
 
-        builder.setBolt("parse", new JSoupParserBolt()).localOrShuffleGrouping(
-                "sitemap");
+                    getPageLinks(ele.attr("abs:href"));
+                }
+            }
 
-        builder.setBolt("index", new StdOutIndexer()).localOrShuffleGrouping(
-                "parse");
+            catch (IOException e) {
 
-        Fields furl = new Fields("url");
-        
-        // can also use MemoryStatusUpdater for simple recursive crawls
-        builder.setBolt("status", new StdOutStatusUpdater())
-                .fieldsGrouping("fetch", Constants.StatusStreamName, furl)
-                .fieldsGrouping("sitemap", Constants.StatusStreamName, furl)
-                .fieldsGrouping("parse", Constants.StatusStreamName, furl)
-                .fieldsGrouping("index", Constants.StatusStreamName, furl);
-
-        return submit("crawl", conf, builder);
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
     }
 }
